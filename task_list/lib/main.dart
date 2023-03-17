81819f9ec98605206b7dfa25a80df322d8f3133c
Daniel Martins de Souza <danielmartsouza@gmail.com>
2023-03-17 11:50:04 -0300

    Adicionando a tela de formulario

--- task_list/lib/main.dart
+++ task_list/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_list/screens/form_screen.dart';
 import 'package:task_list/screens/tela_inicial.dart';
 
 void main() {
@@ -22,7 +23,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: InitialScreen(),
+      home: FormScreen(),
     );
   }
 }
