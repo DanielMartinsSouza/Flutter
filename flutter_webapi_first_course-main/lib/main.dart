d9b6f861f33e99c482b3f125375391e7499a9607
Daniel Martins de Souza <danielmartsouza@gmail.com>
2023-07-05 15:35:25 -0300

    Estudando flutter com integração de Web API

--- flutter_webapi_first_course-main/lib/main.dart
+++ flutter_webapi_first_course-main/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/helpers/async_study.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/add_journal_screen/add_journal_screen.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
+
+  JournalService service = JournalService();
+  //service.register("Olá mundo!");
+  //service.get();
+
+  asyncStudy();
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +22,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (settings){
+        if(settings.name == "add-journal"){
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(builder: (context){
+            return AddJournalScreen(journal: journal);
+          });
+        }
       },
     );
   }
